77bdfc3931fe2dc9a2a9abca21f5bbb5070b9938
StLeoX <stleox@qq.com>
2024-11-20 16:15:53 +0800

    拼接 update 语句。

--- src/task/seeflow_tracing.py
+++ src/task/seeflow_tracing.py
@@ -17,41 +17,40 @@ def update_children(time_batch_spans):
     :return:
     """
 
-    for s in time_batch_spans.values():
-        child_candidates = find_child_candidates(s)
+    for span in time_batch_spans.values():
+        child_candidates = find_child_candidates(span)
+        parent_span_id = span.span_id
 
         # todo 检查 span 之间的 child_candidates 的重叠情况。根据定义是不允许重叠的。当然这里已经用 empty(ParentSpanId) 过滤过。
 
-        for cc in child_candidates:
-            update_parent_span_id(cc, s.span_id, time_batch_spans)
+        updates_sql = ";"
+        for child_span_id in child_candidates:
+            # 先更新缓存
+            if child_span_id in time_batch_spans.keys():
+                time_batch_spans[child_span_id].parent_span_id = parent_span_id
+            # 后更新DB
+            updates_sql += f"ALTER TABLE {t_trace} UPDATE ParentSpanId = {parent_span_id} WHERE SpanId = {child_span_id};"
+        pandas.read_sql_query(updates_sql, ch_engine)
 
 
 def find_child_candidates(parent: Span):
-    sql = f"SELECT SpanId " \
-          f"FROM {t_trace} " \
-          f"WHERE empty(ParentSpanId) " \
-          f"AND HostAddr = {parent.callee} " \
-          f"AND Timestamp > {parent.start_time} " \
-          f"AND Timestamp + Duration < {parent.end_time} " \
-          f"AND EXISTS (" \
-          f"SELECT * FROM {t_l7ss} WHERE " \
-          f"{t_l7ss}.Timestamp > {parent.start_time} " \
-          f"AND {t_l7ss}.Timestamp + {t_l7ss}.Duration < {parent.end_time} " \
-          f"AND {t_l7ss}.TgidRead = {t_trace}.SpanAttributes['tgid_req_cs'] " \
-          f"AND {t_l7ss}.TgidWrite = {t_trace}.SpanAttributes['tgid_resp_cs'] " \
-          f")"
+    find_sql = f"SELECT SpanId " \
+               f"FROM {t_trace} " \
+               f"WHERE empty(ParentSpanId) " \
+               f"AND HostAddr = {parent.callee} " \
+               f"AND Timestamp > {parent.start_time} " \
+               f"AND Timestamp + Duration < {parent.end_time} " \
+               f"AND EXISTS (" \
+               f"SELECT * FROM {t_l7ss} WHERE " \
+               f"{t_l7ss}.Timestamp > {parent.start_time} " \
+               f"AND {t_l7ss}.Timestamp + {t_l7ss}.Duration < {parent.end_time} " \
+               f"AND {t_l7ss}.TgidRead = {t_trace}.SpanAttributes['tgid_req_cs'] " \
+               f"AND {t_l7ss}.TgidWrite = {t_trace}.SpanAttributes['tgid_resp_cs'] " \
+               f")"
 
-    child_candidates_df = pandas.read_sql_query(sql, ch_engine)
+    child_candidates_df = pandas.read_sql_query(find_sql, ch_engine)
 
     child_candidates_sid_list = []
     for cc in child_candidates_df:
         child_candidates_sid_list.append(cc['SpanId'])
     return child_candidates_sid_list
-
-
-def update_parent_span_id(span_id, parent_span_id, sid_span_map):
-    if span_id in sid_span_map.keys():
-        sid_span_map[span_id].parent_span_id = parent_span_id
-
-    sql = f"ALTER TABLE {t_trace} UPDATE ParentSpanId = {parent_span_id} WHERE SpanId = {span_id}"
-    pandas.read_sql_query(sql, ch_engine)
--- src/task/update_trace_id.py
+++ src/task/update_trace_id.py
@@ -1,5 +1,6 @@
 """
 自底向上更新 trace_id 信息。
+该 task 的前提是，各个 ParentSpanId 都被设好了，无论采用何种方式。
 """
 
 import pandas
@@ -17,12 +18,15 @@ def update_trace_ids(time_batch_spans):
     :return:
     """
 
+    updates_sql = ";"
     for s in time_batch_spans.values():
         root_span_id = up_find_root_span_id(s, time_batch_spans)
         if root_span_id != '':
-            update_trace_id(s.span_id, root_span_id)
+            updates_sql += f"ALTER TABLE {t_trace} UPDATE TraceId = {root_span_id} WHERE SpanId = {s.span_id};"
+    pandas.read_sql_query(updates_sql, ch_engine)
 
     # todo history time-batch 中的 span 如何找到并且更新 trace_id？
+    # todo 或者说如何理解 update1 与 update2 之间的延迟？两者在逻辑上是完全异步的，只能说延迟越小越能命中内存。
 
 
 def up_find_root_span_id(span: Span, sid_span_map):
@@ -43,11 +47,6 @@ def up_find_root_span_id(span: Span, sid_span_map):
     if current_span_id != span.span_id:  # 至少向上跳一步，才设置 trace_id
         return current_span_id  # found root
     return ''
-
-
-def update_trace_id(span_id, trace_id):
-    sql = f"ALTER TABLE {t_trace} UPDATE TraceId = {trace_id} WHERE SpanId = {span_id}"
-    pandas.read_sql_query(sql, ch_engine)
 
 
 def query_parent_span_id(span_id):
